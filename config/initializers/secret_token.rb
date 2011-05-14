9315cf79c0ded038d856b16687e743206b39f22a
Eduardo Vasconcelos <eduardooliveiravasconcelos>
2011-05-14 11:21:52 -0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrun::Application.config.secret_token = 'f04c39dedd058910bdfb6493c176c21b74e78883b479ac2998158c6904c8fe20ddae75a8a6f1c327bc1a38bda9b39a2baa7d2ac5deb2d0cbce08eedbc5134705'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Myrun::Application.config.session_store :cookie_store, :key => '_myrun_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Myrun::Application.config.session_store :active_record_store
