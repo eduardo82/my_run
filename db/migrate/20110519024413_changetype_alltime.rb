f7e2808f39184a93d81cefa5fe8829025e0cd951
Eduardo Vasconcelos <eduardooliveiravasconcelos>
2011-05-26 16:59:46 -0300

    Changing

--- db/migrate/20110519024413_changetype_alltime.rb
+++ db/migrate/20110519024413_changetype_alltime.rb
@@ -1,10 +1,13 @@
 class ChangetypeAlltime < ActiveRecord::Migration
   def self.up
-    change_table :runs do |t|
-      t.change(:alltime, :integer)
-    end
-  end
+     change_table :runs do |t|
+       t.change :alltime, :integer
+     end
+   end
 
   def self.down
+    change_table :runs do |t|
+      t.change :alltime, :float
+    end
   end
 end
